a48993d252dad80bcf5ce498fb544f05098c84a0
Houssien Zeineddine <houssienzd@hotmail.com>
2025-04-25 19:11:04 +0300

    implemeting the landing page cards part

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import politician from "../../assets/Politician giving his speech to public.svg";
+import login from "../../assets/Login.svg";
+import vote from "../../assets/online vote.svg";
+import viewResults from "../../assets/card view result.svg";
 import Button from "../../components/Button";
 import { useNavigate } from "react-router-dom";
 import "./styles.css";
@@ -29,8 +32,48 @@ const Home = () => {
           />
         </div>
       </div>
-      <div className="landing-page-cards">
-        <p>landing page cards</p>
+      <div className="landing-page-cards-container">
+        <div className="landing-page-cards">
+          <div className="card">
+            <div className="card-text">
+              <p>step 1</p>
+              <h4>Register</h4>
+              <p>Create an account to be able to vote</p>
+            </div>
+            <div>
+              <img src={login} alt="Login" />
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-text">
+              <p>step 2</p>
+              <h4>Vote</h4>
+              <p>Vote for your prefered candidate</p>
+            </div>
+            <div>
+              <img src={vote} alt="Vote Online" />
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-text">
+              <p>step 3</p>
+              <h4>View Results</h4>
+              <p>View election results</p>
+            </div>
+            <div>
+              <img src={viewResults} alt="View Results" />
+            </div>
+          </div>
+        </div>
+        <div className="landing-page-cards-text">
+          <h4>
+            Make your decision-making process more modern, safe, and efficient
+          </h4>
+          <p>
+            "Make the shift from manual ballot voting to a secure online
+            system—without compromising the integrity of your vote."
+          </p>
+        </div>
       </div>
       <div className="our-features">
         <p>our features</p>
