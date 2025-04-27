0ec977825fffabd495906835cd2f21aca5abe8bd
Houssien Zeineddine <houssienzd@hotmail.com>
2025-04-27 15:24:52 +0300

    implementing the right half of registration page

--- client/src/pages/Register/index.jsx
+++ client/src/pages/Register/index.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import Button from "../../components/Button";
+import welcomeIllustration from "../../assets/register illautration.svg";
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register</h1>
-      <p>Register page content goes here.</p>
+    <div className="login-page-container">
+      <div></div>
+      <div className="description-container">
+        <div className="description-wrapper">
+          <Button
+            text="Home"
+            variant="transparent"
+            size="small"
+            onClick={() => {
+              navigate("/");
+            }}
+          />
+          <div className="inside-description-container">
+            <h1>Welcome!</h1>
+            <p>
+              Welcome to Secure Remote Voting system. Register to vote for your
+              preferred candidate
+            </p>
+            <img src={welcomeIllustration} alt="Secure Web" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
