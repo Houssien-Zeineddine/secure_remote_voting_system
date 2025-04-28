f7c3697c9782dca8d90f3399efb6852c9e041c9a
Houssien Zeineddine <houssienzd@hotmail.com>
2025-04-29 00:03:19 +0300

    implementing dashboard description

--- client/src/pages/Dashboard/Elections/index.jsx
+++ client/src/pages/Dashboard/Elections/index.jsx
@@ -1,9 +1,48 @@
 import React from "react";
+import Button from "../../../components/Button";
+import politician from "../../../assets/Politician giving his speech to public.svg";
+import "./style.css";
 
 const Elections = () => {
+  const registeredVoters = 200; //getting voters number from backend
+  const totalVotes = 220; //getting voters number from backend
+  const malisciousVotes = 20; //getting voters number from backend
   return (
-    <div>
-      <p>Elections</p>
+    <div className="elections-dashboard-container">
+      <div className="description-live-results-container">
+        <div className="dashboard-description-container">
+          <div className="dashboard-description">
+            <h4>Ongoing Elections</h4>
+            <h2>Elections Name</h2>
+            <Button
+              text="Vote Now"
+              variant="blue"
+              size="small"
+              onClick={() => navigate("/vote")}
+              className="vote-now-btn"
+            />
+          </div>
+          <img src={politician} alt="Politician giving his speech to public" />
+        </div>
+        <div className="dashboard-live-results">
+          <p>result</p>
+        </div>
+      </div>
+      <div className="statistics-container">
+        <h4>Voting Process</h4>
+        <div className="round-div registered-voter">
+          <p>{registeredVoters}</p>
+        </div>
+        <p>Total Number of Registered Voters</p>
+        <div className="round-div total-votes">
+          <p>{totalVotes}</p>
+        </div>
+        <p>Total Number of Votes</p>
+        <div className="round-div maliscious-votes">
+          <p>{malisciousVotes}</p>
+        </div>
+        <p>Total Number of Malicious votes</p>
+      </div>
     </div>
   );
 };
--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -4,7 +4,7 @@ import NoElections from "../../components/NoElections";
 import "./style.css";
 
 const Dashboard = () => {
-  const elections = false;
+  const elections = true; //calling api to get elections
   return (
     <div className="along-sidebar-positioning">
       {elections ? <Elections /> : <NoElections />}
