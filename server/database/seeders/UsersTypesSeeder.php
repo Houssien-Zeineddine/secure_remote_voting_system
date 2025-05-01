94ef1d20b9842ee342b006790ce88442ddb798b7
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-01 17:04:29 +0300

    creating seeder for users types

---
+++ server/database/seeders/UsersTypesSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTypesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users_types')->insert([
+            ['user_type'=>'admin'],
+            ['user_type'=>'candidate'],
+            ['user_type'=>'user'],
+        ]);
+    }
+}
