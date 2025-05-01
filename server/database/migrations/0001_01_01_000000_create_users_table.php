b66ad7b8c1673eac90d85b29b82fefbdbb8d782f
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-01 10:38:58 +0300

    modifying users migrations

--- server/database/migrations/0001_01_01_000000_create_users_table.php
+++ server/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->date('birthday');
+            $table->integer('id_number')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profile_picture_path');
             $table->rememberToken();
             $table->timestamps();
         });
