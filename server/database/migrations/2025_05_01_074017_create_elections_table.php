a77b2a7bafad95ccdbd62c8992c714ea0b694e50
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-01 10:49:50 +0300

    creating elections table migration

---
+++ server/database/migrations/2025_05_01_074017_create_elections_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('elections', function (Blueprint $table) {
+            $table->id();
+            $table->integer('regions_id')->constrained('regions');
+            $table->string('title');
+            $table->string('description');
+            $table->boolean('ongoing');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('elections');
+    }
+};
