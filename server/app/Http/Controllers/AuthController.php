3a8f6ca798d8fc52551a9d559ee4168e39042160
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-05 20:51:28 +0300

    adding migration to update users tyable to set user type to 3 as default

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -12,11 +12,6 @@ use App\Services\AuthService;
 
 class AuthController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth:api', ['except' => ['login','register']]);
-    }
-
     public function register(RegisterationRequest $request) {
         $registerUser = new AuthService();
         $newUser = $registerUser->registerUser($request);
