fc638d45f40b2436515357a6657d373d29e2c0b8
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-05 12:00:36 +0300

    creating authcontroller

--- server/routes/api.php
+++ server/routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(["prefix" => "v0.1"], function () {
+    
+    //login and register public routes, no need for authentication
+    Route::group(["prefix" => "guest"], function(){
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
+    });
+});
