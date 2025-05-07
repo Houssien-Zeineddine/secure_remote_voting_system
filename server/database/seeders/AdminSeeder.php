bb53587c9ef7098dfa61ceba1646e1362ba3c785
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-07 10:48:48 +0300

    creating user seeder

--- server/database/seeders/AdminSeeder.php
+++ server/database/seeders/AdminSeeder.php
@@ -13,8 +13,7 @@ class AdminSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
+    public function run(): void {
         $adminEmail = 'test@example.com';
 
         $existingUser = User::where('email', $adminEmail)->first();
