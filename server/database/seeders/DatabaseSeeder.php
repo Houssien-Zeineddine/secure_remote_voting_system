a95d1fe0df8874191c8eadbca50e0bd716c5cb28
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-07 11:10:22 +0300

    adding a flag to check if an ongoing elections exixts not o create a new one

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -12,14 +12,16 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
+    public function run(): void {
       DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         
       $this->call([
         UsersTypesSeeder::class,
         AdminSeeder::class,
-        RegionsSeeder::class,    
+        RegionsSeeder::class, 
+        ElectionsSeeder::class,
+        CountedVoteSeeder::class,
+        MaliciousVoteSeeder::class   
       ]); 
         
       DB::statement('SET FOREIGN_KEY_CHECKS=1;');
