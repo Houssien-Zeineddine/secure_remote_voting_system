593a28450507601fb0185694b915a2ca3a87a748
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-08 17:45:54 +0300

    creating update candidate in user service and adding authorization adn authentication checks

--- server/app/Services/UserService.php
+++ server/app/Services/UserService.php
@@ -11,20 +11,40 @@ class UserService {
      * Create a new class instance.
      */
     public function updateUser (EditProfileRequest $request) {
+        $user = Auth::user();
 
-        $updatedUser = Auth::user();
+        if ($user->id !== $request->user_id) {
+            abort(403, 'Unauthorized to update this user');
+        }
         
-        $updatedUser->first_name = $request->first_name;
-        $updatedUser->middle_name = $request->middle_name;
-        $updatedUser->last_name = $request->last_name;
-        $updatedUser->birthday = $request->birthday;
-        $updatedUser->id_number = $request->id_number;
-        $updatedUser->update();
+        $user->first_name = $request->first_name;
+        $user->middle_name = $request->middle_name;
+        $user->last_name = $request->last_name;
+        $user->birthday = $request->birthday;
+        $user->id_number = $request->id_number;
+        $user->save();
 
-        return $updatedUser;
+        return $user;
     }
 
     public function getCandidates () {
         return User::where('user_type', 2)->get();
+    }
+
+    public function updateCandidate (RegisterationRequest $request) {
+        $admin = Auth::user();
+        if($admin->id !== 1) {
+            abort(403, 'Cannot update other users');
+        }
+
+        $candidate = User::FindOrFail($request->id);
+        if ($candidate->user_type !== 2) { 
+            abort(400, 'Not a candidate');
+        }
+
+        $candidate->user_type = 3;
+        $candidate->save();
+
+        return $candidate;
     }
 }
