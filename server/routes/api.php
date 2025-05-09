54f5e792446dec07ab2e58f03be5e691813ef415
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-09 10:07:07 +0300

    creating rpute to add elections

--- server/routes/api.php
+++ server/routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\SettingsController;
 use App\Http\Controllers\AddCampaignController;
 use App\Http\Controllers\AddElectionsController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ElectionsController;
 use App\Http\Middleware\CheckUserType;
 
 Route::group(['prefix' => 'v0.1'], function () {
@@ -38,6 +39,7 @@ Route::group(['prefix' => 'v0.1'], function () {
                         Route::post('/addelections', [AddElectionsController::class, 'store']);
                         Route::put('/candidates', [UserController::class, 'updateCandidate']);
                         Route::put('/addcandidate', [UserController::class, 'addCandidate']);
+                        Route::delete('/deleteelections', [ElectionsController::class, 'deleteElections']);
                     });
                 });
             }); 
