396987119696ee6e52b0e02dd7539a17a334ebbd
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-10 17:48:46 +0300

    fetching campaigns in check campaigns context and exporting it to children

--- client/src/components/context/CheckCampaignContext.jsx
+++ client/src/components/context/CheckCampaignContext.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axiosBaseUrl from "../../Utils/axios";
 
 export const CheckCampaignContext = useContext();
 
 export const CheckCampaignProvider = ({ children }) => {
-  const [campaign, setCampaign] = useState(null);
+  const [campaigns, setCampaigns] = useState(null);
   const access_token = localStorage.getItem("access_token");
 
-  //   const fetchCampaigns =
+  const fetchCampaigns = async () => {
+    try {
+      const response = await axiosBaseUrl.get("user/getcampaigns", {
+        headers: { Authorization: `Bearer ${access_token}` },
+      });
+
+      const campaignsArray = response.data;
+
+      if (campaignsArray) {
+        setCampaigns(campaignsArray);
+      } else {
+        setCampaigns(null);
+      }
+    } catch (error) {
+      console.log(error);
+      setCampaigns(null);
+    }
+  };
+
+  useEffect(() => {
+    fetchCampaigns();
+  }, []);
+
+  return (
+    <CheckCampaignContext.Provider value={{ campaigns }}>
+      {children}
+    </CheckCampaignContext.Provider>
+  );
 };
