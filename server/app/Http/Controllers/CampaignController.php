e5ecc5202b8a7285d50c649319228b0bde8a63c8
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-10 10:53:02 +0300

    adding the add campaign check to campaign controller

--- server/app/Http/Controllers/CampaignController.php
+++ server/app/Http/Controllers/CampaignController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\CampaignService;
+use App\Http\Requests\AddCampaignRequest;
 
 class CampaignController extends Controller {
-    public function addCampaign(Request $request) {
+    public function addCampaign(AddCampaignRequest $request) {
         $campaign = new CampaignService;
 
         $addedCampaign = $campaign->addCampaign($request);
