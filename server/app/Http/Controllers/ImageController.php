56e3ef7acaebf79a5fdffe8b4d06ce0e344aefb2
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-12 11:12:06 +0300

    creating image service

--- server/app/Http/Controllers/ImageController.php
+++ server/app/Http/Controllers/ImageController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ImageUploadRequest;
 
 class ImageController extends Controller {
-    
+    public function upload (ImageUploadRequest $request) {
+        
+    }
 }
