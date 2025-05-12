cf3737b2e19648c7b265865ed883790dce108d06
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-12 11:24:57 +0300

    implementing the image upload service logic

--- server/app/Services/ImageService.php
+++ server/app/Services/ImageService.php
@@ -2,13 +2,35 @@
 
 namespace App\Services;
 
-class ImageService
-{
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class ImageService {
     /**
      * Create a new class instance.
      */
-    public function __construct()
-    {
-        //
+    
+     public function uploadImage (Request $request) {
+        $image = $request->file('image');
+
+        $folder = 'images/' . date('Y/m');
+
+        $filename = Str::uuid() . '.' . $image->getClientOriginalExtension();
+        
+        $path = Storage::disk('public')->putFileAs(
+            $folder,
+            $image,
+            $filename
+        );
+
+        $post = new Post();
+        $post->image_path = $path;
+        $post->save();
+
+        return response()->json([
+            'success' => true,
+            'url' => Storage::disk('public')->url($path),
+            'path' => $path,
+        ]);
     }
 }
