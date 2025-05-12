120c6bee70adfd61aff4d023e7fc63215570f770
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-12 11:14:10 +0300

    writing the image controller logic

--- server/app/Http/Controllers/ImageController.php
+++ server/app/Http/Controllers/ImageController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\ImageUploadRequest;
+use App\Services\ImageService;
+
 
 class ImageController extends Controller {
     public function upload (ImageUploadRequest $request) {
+        $image = new ImageService;
         
+        $uploadedImage = $image->uploadImage();
+
+        return $this->successResponse($uploadedImage, 200);
     }
 }
