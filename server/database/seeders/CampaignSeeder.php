7927e592ee0880d886dfa60f334c04072ecd75bc
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-14 00:27:37 +0300

    adding use model campaign to campaign seeder

--- server/database/seeders/CampaignSeeder.php
+++ server/database/seeders/CampaignSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Elections;
+use App\Models\Campaign;
 use App\Models\User;
 
 class CampaignSeeder extends Seeder {
