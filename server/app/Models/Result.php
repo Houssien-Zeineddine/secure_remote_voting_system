198f74de9915f523bd6a6d24f69bd4e335512084
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-13 16:01:15 +0300

    adding fillables to result model

--- server/app/Models/Result.php
+++ server/app/Models/Result.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Result extends Model
-{
-    //
+
+class Result extends Model{
+    use SoftDeletes;
+    
+    protected $fillable = [
+        'user_id',
+        'elections_id',
+        'candidate_id',
+        'counted_votes'
+    ];
 }
