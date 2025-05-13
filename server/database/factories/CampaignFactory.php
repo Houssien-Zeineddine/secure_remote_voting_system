d5b6a6a904b8be9313bcf5843b9c454a10baffb6
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-14 00:12:57 +0300

    writing campaign factory commit

--- server/database/factories/CampaignFactory.php
+++ server/database/factories/CampaignFactory.php
@@ -2,22 +2,25 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+use App\Models\Elections;
+use App\Models\Campaign;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Campaign>
  */
-class CampaignFactory extends Factory
-{
+class CampaignFactory extends Factory {
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
+    public function definition(): array {
         return [
-            //
+            'user_id' => User::factory(),
+            'elections_id' => Elections::factory(),
+            'campaign' => Campaign::factory()
         ];
     }
 }
