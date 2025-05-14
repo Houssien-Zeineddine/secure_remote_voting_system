107335f4834857c62d45d72f2bdb8d0031a42b0e
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-14 22:41:05 +0300

    setting the lng and lat for every region

--- server/app/Services/LeabanonRegionsService.php
+++ server/app/Services/LeabanonRegionsService.php
@@ -2,13 +2,46 @@
 
 namespace App\Services;
 
-class LeabanonRegionsService
-{
+class LeabanonRegionsService {
     /**
      * Create a new class instance.
      */
-    public function __construct()
-    {
-        //
-    }
+    private const REGIONS = [
+        'beirut' => [
+            'min_lat' => 33.85,
+            'max_lat' => 33.93,
+            'min_lng' => 35.47,
+            'max_lng' => 35.55
+        ],
+        'south' => [
+            'min_lat' => 33.10,
+            'max_lat' => 33.60,
+            'min_lng' => 35.10,
+            'max_lng' => 35.60
+        ],
+        'mount_lebanon' => [
+            'min_lat' => 33.70,
+            'max_lat' => 34.20,
+            'min_lng' => 35.40,
+            'max_lng' => 35.90
+        ],
+        'bekaa' => [
+            'min_lat' => 33.40,
+            'max_lat' => 34.40,
+            'min_lng' => 35.80,
+            'max_lng' => 36.40
+        ],
+        'north' => [
+            'min_lat' => 34.20,
+            'max_lat' => 34.70,
+            'min_lng' => 35.60,
+            'max_lng' => 36.40
+        ],
+        'whole_country' => [
+            'min_lat' => 33.05,
+            'max_lat' => 34.70,
+            'min_lng' => 35.10,
+            'max_lng' => 36.60
+        ]
+    ];
 }
