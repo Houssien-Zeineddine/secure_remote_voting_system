9e47a2f250e1896110ee8329be990fa74d4a6167
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-14 22:12:20 +0300

    creating filter vote method

--- server/app/Services/VoteService.php
+++ server/app/Services/VoteService.php
@@ -2,9 +2,13 @@
 
 namespace App\Services;
 
+use Illuminate\Http\Request;
+
 class VoteService {
     /**
      * Create a new class instance.
      */
-    
+    public function filterVote(Request $request) {
+        
+    }
 }
