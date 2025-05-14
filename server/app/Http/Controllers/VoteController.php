9c2d9daae946108c718bbaf073f0ed39ee2f54f0
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-14 17:32:55 +0300

    writing vote to candidate method in cote controller

--- server/app/Http/Controllers/VoteController.php
+++ server/app/Http/Controllers/VoteController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class VoteController extends Controller
-{
-    //
+class VoteController extends Controller {
+    public function voteToCandidate (Request $rewuest) {
+        $vote = new VoteService();
+
+        $filteredVote = $vote->filterVote($request);
+
+        return $this->successResponse($filteredVote, 200);
+    }
 }
