a1239faa4b35d87c3c69f7263a48cd920375072f
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-15 11:58:30 +0300

    modifying auth cintext to handle staying in current page when refeeshing

--- client/src/components/Context/AuthorizationContext.jsx
+++ client/src/components/Context/AuthorizationContext.jsx
@@ -1,31 +1,35 @@
 import React, { createContext, useState, useEffect } from "react";
 import axiosBaseUrl from "../../Utils/axios";
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null); //used in navbar to determine what button to render whether register or logout
   const [authLoading, setAuthLoading] = useState(true);
+  const navoigate = useNavigate();
+
+  const verifyToken = async () => {
+    const token = localStorage.getItem("access_token");
+    if (!token) {
+      setAuthLoading(false);
+      return;
+    }
+
+    try {
+      const response = await axiosBaseUrl.get("/me", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      setUser(response.data);
+    } catch (error) {
+      localStorage.removeItem("access_token");
+      setUser(null);
+    } finally {
+      setAuthLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const verifyToken = async () => {
-      const token = localStorage.getItem("access_token");
-      if (token) {
-        try {
-          const response = await axiosBaseUrl.get("/me", {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-          setUser(response.data);
-        } catch (err) {
-          localStorage.removeItem("access_token");
-          setUser(null);
-        }
-      }
-      setAuthLoading(false);
-    };
-
     verifyToken();
   }, []);
 
