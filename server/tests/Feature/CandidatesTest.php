9b3a5d307c84e4fe65213bd2e1c8700b06ea8672
Houssien Zeineddine <houssienzd@hotmail.com>
2025-05-18 12:30:28 +0300

    testing get candidates successfully

--- server/tests/Feature/CandidatesTest.php
+++ server/tests/Feature/CandidatesTest.php
@@ -2,19 +2,48 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use App\Models\UserType;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
-class CandidatesTest extends TestCase
-{
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+class CandidatesTest extends TestCase {
+    use RefreshDatabase;
+
+    public function test_authenticated_user_can_get_all_candidates() {
+        $this->seed(\UsersTypesSeeder::class);
+
+        $authUser = User::create([
+            'first_name' => 'Auth',
+            'middle_name' => 'Test',
+            'last_name' => 'User',
+            'birthday' => '1990-01-01',
+            'id_number' => '111222333',
+            'email' => 'authuser@example.com',
+            'password' => Hash::make('password'),
+            'user_type' => 3, 
+        ]);
+
+        $loginResponse = $this->postJson('/api/v0.1/guest/login', [
+            'email' => 'authuser@example.com',
+            'password' => 'password',
+        ]);
+
+        $token = $loginResponse->json('access_token');
+
+        $candidates = User::factory()->count(6)->create([
+            'user_type' => 2 
+        ]);
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->getJson('/api/v0.1/user/candidates');
 
         $response->assertStatus(200);
+        $response->assertJsonCount(6); 
+        $response->assertJsonFragment([
+            'user_type' => 2
+        ]);
     }
 }
